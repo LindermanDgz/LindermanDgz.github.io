c2e3786ba0f1d6525472ebb72b4bdf811400d98a
Linderman Dominguez <euforia.dgz@gmail.com>
2024-08-01 02:40:50 -0600

    Add basic components

--- components.d.ts
+++ components.d.ts
@@ -7,6 +7,10 @@ export {}
 /* prettier-ignore */
 declare module 'vue' {
   export interface GlobalComponents {
+    ContactInfo: typeof import('./src/components/ContactInfo.vue')['default']
+    Education: typeof import('./src/components/Education.vue')['default']
+    Experience: typeof import('./src/components/Experience.vue')['default']
     HelloWorld: typeof import('./src/components/HelloWorld.vue')['default']
+    Stack: typeof import('./src/components/Stack.vue')['default']
   }
 }
---
+++ src/components/Experience.vue
@@ -0,0 +1,43 @@
+<template>
+    <v-timeline side="end">
+        <v-timeline-item v-for="(role, i) in roles" :key="i" dot-color="blue-lighten-3" icon="mdi-star" size="large"
+            fill-dot>
+            <template v-slot:opposite>
+                <div class="pt-1 headline font-weight-light" v-text="role.date"></div>
+            </template>
+            <div>
+                <h2 class="mt-n1 headline font-weight-light mb-4" v-text="role.name"></h2>
+                <div v-text="role.company"> </div>
+            </div>
+        </v-timeline-item>
+    </v-timeline>
+</template>
+
+<script>
+export default {
+    data: () => ({
+        roles: [
+            {
+                name: 'Software Engineer',
+                company: 'Intel Corporation',
+                date: 'Jan 2022 - Jul 2023',
+            },
+            {
+                name: 'Graduate Technical Intern',
+                company: 'Intel Corporation',
+                date: 'Nov 2020 - Nov 2021',
+            },
+            {
+                name: 'Back End Python Developer',
+                company: 'Universidad Intercultural de Chiapas',
+                date: 'Sep 2020 - Dec 2020',
+            },
+            {
+                name: 'Fullstack Developer Angular',
+                company: 'Adiante Apps',
+                date: 'May 2020 - Sep 2020',
+            },
+        ],
+    }),
+}
+</script>
