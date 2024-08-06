3562bf1286cf0c1330976bef5b5be7ca074fe49a
Linderman Dominguez <euforia.dgz@gmail.com>
2024-08-05 22:22:50 -0600

    Toggle dark/light theme

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,25 @@
 <template>
-  <v-app>
+  <v-app :theme="theme">
+    <v-fab location="top end" :icon="icon" @click="toggleTheme" icon-color="red" absolute app appear></v-fab>
     <v-main>
       <Resume />
     </v-main>
   </v-app>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      theme: 'dark',
+      icon: 'mdi-white-balance-sunny',
+    }
+  },
+  methods: {
+    toggleTheme() {
+      this.theme = this.theme === 'light' ? 'dark' : 'light';
+      this.icon = this.theme === 'light' ? 'mdi-weather-night' : 'mdi-white-balance-sunny'
+    }
+  }
+}
+</script>
