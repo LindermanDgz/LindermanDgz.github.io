dfdb83ee35561e26281ea848288a881f4c712049
Linderman Dominguez <euforia.dgz@gmail.com>
2024-08-05 18:58:48 -0600

    Enhance experience timeline

--- src/components/Experience.vue
+++ src/components/Experience.vue
@@ -1,43 +1,72 @@
 <template>
-    <v-timeline side="end">
-        <v-timeline-item v-for="(role, i) in roles" :key="i" dot-color="blue-lighten-3" icon="mdi-star" size="large"
-            fill-dot>
-            <template v-slot:opposite>
-                <div class="pt-1 headline font-weight-light" v-text="role.date"></div>
-            </template>
-            <div>
-                <h2 class="mt-n1 headline font-weight-light mb-4" v-text="role.name"></h2>
-                <div v-text="role.company"> </div>
-            </div>
-        </v-timeline-item>
-    </v-timeline>
+    <v-container>
+        <v-row>
+            <v-col cols="12" class="text-center">
+                Experience
+            </v-col>
+            <v-col cols="12" lg="1" xl="2" xxl="3"> </v-col>
+            <v-col>
+                <v-timeline side="end" align="start" truncate-line="start">
+                    <v-timeline-item v-for="(role, i) in roles" :key="i" dot-color="blue-lighten-3" fill-dot
+                        icon="mdi-briefcase" size="large"
+                        :icon-color="$vuetify.theme.name === 'light' ? 'white' : 'black'" width="100%">
+                        <template v-slot:opposite>
+                            <v-card-text v-text="role.date">
+                            </v-card-text>
+                        </template>
+                        <v-card variant="tonal">
+                            <v-card-title v-text="role.name">
+                            </v-card-title>
+                            <v-card-subtitle>
+                                <v-icon icon="mdi-domain"></v-icon> <v-text v-text="role.company"></v-text>
+                                <v-spacer></v-spacer>
+                                <v-icon icon="mdi-map-marker"></v-icon> <v-text v-text="role.work_model"></v-text>
+                            </v-card-subtitle>
+                            <v-card-text v-text="role.description"></v-card-text>
+                        </v-card>
+                    </v-timeline-item>
+                </v-timeline>
+            </v-col>
+            <v-col cols="12" lg="1" xl="2" xxl="3"> </v-col>
+        </v-row>
+    </v-container>
 </template>
 
 <script>
 export default {
-    data: () => ({
-        roles: [
-            {
-                name: 'Software Engineer',
-                company: 'Intel Corporation',
-                date: 'Jan 2022 - Jul 2023',
-            },
-            {
-                name: 'Graduate Technical Intern',
-                company: 'Intel Corporation',
-                date: 'Nov 2020 - Nov 2021',
-            },
-            {
-                name: 'Back End Python Developer',
-                company: 'Universidad Intercultural de Chiapas',
-                date: 'Sep 2020 - Dec 2020',
-            },
-            {
-                name: 'Fullstack Developer Angular',
-                company: 'Adiante Apps',
-                date: 'May 2020 - Sep 2020',
-            },
-        ],
-    }),
+    data() {
+        return {
+            roles: [
+                {
+                    name: 'Software Engineer',
+                    company: 'Intel Corporation',
+                    date: 'Jan 2022 - Jul 2023',
+                    description: "RTL Development and CI/CD",
+                    work_model: "Remote",
+                },
+                {
+                    name: 'Graduate Technical Intern',
+                    company: 'Intel Corporation',
+                    date: 'Nov 2020 - Nov 2021',
+                    description: "RTL Development and CI/CD",
+                    work_model: "Remote",
+                },
+                {
+                    name: 'Back End Python Developer',
+                    company: 'Universidad Intercultural de Chiapas',
+                    date: 'Sep 2020 - Dec 2020',
+                    description: "RESTful API development with Django framework RESTful API development with Django framework RESTful API development with Django framework",
+                    work_model: "Remote",
+                },
+                {
+                    name: 'Fullstack Developer Angular',
+                    company: 'Adiante Apps',
+                    date: 'May 2020 - Sep 2020',
+                    description: "WEB and Mobile application development using Angular",
+                    work_model: "Remote",
+                },
+            ],
+        }
+    },
 }
 </script>
