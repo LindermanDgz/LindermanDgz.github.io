0a9ed6fd0010ab8e6d233a4afb74113497d3de03
Linderman Dominguez <euforia.dgz@gmail.com>
2024-08-06 21:44:26 -0600

    Enhance education component
    - Fix degree name in second element
    - Enhance image container (remove use of avatar)
    - Add degree icons

--- src/components/Education.vue
+++ src/components/Education.vue
@@ -8,16 +8,15 @@
             <v-col>
                 <v-card variant="flat">
                     <v-row class="text-center" align="stretch" justify="center">
-                        <v-col>
-                            <v-avatar class="ma-3" rounded="0" size="100">
-                                <v-img v-if="$vuetify.theme.name === 'light'" src="@/assets/unach_light.png"></v-img>
-                                <v-img v-else src="@/assets/unach.svg"></v-img>
-                            </v-avatar>
+                        <v-col cols="3">
+                            <v-img v-if="$vuetify.theme.name === 'light'" src="@/assets/unach_light.png"></v-img>
+                            <v-img v-else src="@/assets/unach.svg"></v-img>
                         </v-col>
                         <v-col>
                             <v-card-title>Universidad Autónoma de Chiapas</v-card-title>
-                            <v-card-subtitle>Aug 2009 - Dec 2014</v-card-subtitle>
-                            <v-card-text>Physics</v-card-text>
+                            <v-card-subtitle>2009 - 2014</v-card-subtitle>
+                            <v-card-text>
+                                <v-icon icon="mdi-rocket-launch"></v-icon> B.S. Physics</v-card-text>
                         </v-col>
                     </v-row>
                 </v-card>
@@ -25,15 +24,14 @@
             <v-col>
                 <v-card variant="flat">
                     <v-row class="text-center" align="stretch" justify="center">
-                        <v-col>
-                            <v-avatar class="ma-3" rounded="50" size="100" color="white">
-                                <v-img src="@/assets/upchiapas.png"></v-img>
-                            </v-avatar>
+                        <v-col cols="3">
+                            <v-img src="@/assets/upchiapas.png" color="white" rounded="circle"></v-img>
                         </v-col>
                         <v-col>
                             <v-card-title>Universidad Politécnica de Chiapas</v-card-title>
-                            <v-card-subtitle>Jan 2017 - Dec 2020</v-card-subtitle>
-                            <v-card-text>Physics</v-card-text>
+                            <v-card-subtitle>2017 - 2020</v-card-subtitle>
+                            <v-card-text>
+                                <v-icon icon="mdi-desktop-classic"></v-icon> B.E. Software Engineering</v-card-text>
                         </v-col>
                     </v-row>
                 </v-card>
