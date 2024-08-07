9bf6cf056d5bbf0143f4c396445a83058e41edf4
Linderman Dominguez <euforia.dgz@gmail.com>
2024-08-06 21:10:20 -0600

    Add tech stack info

--- src/components/Stack.vue
+++ src/components/Stack.vue
@@ -1,9 +1,35 @@
 <template>
     <v-container>
         <v-row>
-            <v-col class="text-center">
+            <v-col cols="12" class="text-center">
                 Stack
             </v-col>
+            <v-col cols="12" lg="1" xl="2" xxl="3"></v-col>
+            <v-col>
+                <v-row align="center" justify="center">
+                    <v-col cols="3">
+                        <v-img src="@/assets/cpp.png"></v-img>
+                    </v-col>
+                </v-row>
+                <v-row align="center" justify="center">C++</v-row>
+            </v-col>
+            <v-col>
+                <v-row align="center" justify="center">
+                    <v-col cols="3">
+                        <v-img src="@/assets/python.png"></v-img>
+                    </v-col>
+                </v-row>
+                <v-row align="center" justify="center">Python</v-row>
+            </v-col>
+            <v-col>
+                <v-row align="center" justify="center">
+                    <v-col cols="3">
+                        <v-img src="@/assets/linux.png"></v-img>
+                    </v-col>
+                </v-row>
+                <v-row align="center" justify="center">Linux</v-row>
+            </v-col>
+            <v-col cols="12" lg="1" xl="2" xxl="3"></v-col>
         </v-row>
     </v-container>
 </template>
